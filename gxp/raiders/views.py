13eb69baa999c98633bfd5ab1396aaf45cc709eb
John Goben <TwinHits@gmail.com>
2022-08-21 13:55:57 -0400

    Added support for query param for active state.

--- gxp/raiders/views.py
+++ gxp/raiders/views.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import viewsets
 
 from gxp.raiders.models import Alias, Raider, Alt
@@ -8,6 +10,13 @@ class RaidersViewSet(viewsets.ModelViewSet):
     queryset = Raider.objects.all()
     serializer_class = RaiderSerializer
 
+    def get_queryset(self):
+        active = self.request.query_params.get("active")
+        if active is not None:
+            active= json.loads(active)
+            self.queryset = self.queryset.filter(active=active)
+
+        return self.queryset
 
 class AltsViewSet(viewsets.ModelViewSet):
     queryset = Alt.objects.all()
