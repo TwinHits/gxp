d17e59d76128618e47e2ec3467b44c6bffe11b88
John Goben <TwinHits@gmail.com>
2022-10-22 14:41:13 -0400

    Fixed timezones for checking raid date.

--- gxp/raids/utils.py
+++ gxp/raids/utils.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-
+from pytz import timezone
 
 class RaidUtils:
     def is_raid_optional(timestamp):
         notOptionalDays = [1, 3]
         raidDate = datetime.fromtimestamp(timestamp / 1000)
+        raidDate = timezone('US/Eastern').localize(raidDate)
         return raidDate.weekday() not in notOptionalDays
