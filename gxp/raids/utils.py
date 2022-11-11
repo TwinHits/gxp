9a9e47181d648edb28e65fafff14910bde10cce2
John Goben <TwinHits@gmail.com>
2022-11-11 12:41:09 -0500

    Refactored timezone handling.

--- gxp/raids/utils.py
+++ gxp/raids/utils.py
@@ -4,6 +4,5 @@ from pytz import timezone
 class RaidUtils:
     def is_raid_optional(timestamp):
         notOptionalDays = [1, 3]
-        raidDate = datetime.fromtimestamp(timestamp / 1000)
-        raidDate = timezone('US/Eastern').localize(raidDate)
+        raidDate = datetime.fromtimestamp(timestamp / 1000, tz=timezone('US/Eastern'))
         return raidDate.weekday() not in notOptionalDays
