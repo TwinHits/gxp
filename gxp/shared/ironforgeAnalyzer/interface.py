dd07d5a33454709a170e42662761d82d27ca4c0f
John Goben <TwinHits@gmail.com>
2023-05-14 11:40:36 -0400

    Updated ironforge analyzer error handling.

--- gxp/shared/ironforgeAnalyzer/interface.py
+++ gxp/shared/ironforgeAnalyzer/interface.py
@@ -7,6 +7,9 @@ class IronforgeAnalyzerInterface:
     def get_consumes_for_report(report_id):
         try:
             response = Api.get(f"https://ironforge.pro/api/analyzer/{report_id}")
+            if not response or not response.get("data"):
+                IronforgeAnalyzerUtils.handle_error(response)
+
             return response.get("data")
         except Exception as err:
             IronforgeAnalyzerUtils.handle_error(err)
