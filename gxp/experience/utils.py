cf6097a11edd99530998eec1f5b616139fc69569
John Goben <TwinHits@gmail.com>
2023-06-10 15:23:28 -0400

    Added support for missing tokens in desription replacement.

--- gxp/experience/utils.py
+++ gxp/experience/utils.py
@@ -1,18 +1,22 @@
 import logging
+from collections import defaultdict
 
 from django.forms import model_to_dict
 
 from gxp.shared.utils import SharedUtils
 
+def default_value_func():
+    return "MISSING_TOKEN"
 
 class ExperienceUtils:
     def get_description_from_template(experience_gain):
-        tokens = {
+        tokens = defaultdict(default_value_func)
+        tokens.update({
             **model_to_dict(experience_gain),
             **model_to_dict(experience_gain.raider),
             **model_to_dict(experience_gain.experienceEvent),
             **experience_gain.tokens,
-        }
+        })
         tokens["date"] = SharedUtils.format_datetime_as_date(
             SharedUtils.get_datetime_from_timestamp(tokens["timestamp"])
         )
@@ -21,7 +25,7 @@ class ExperienceUtils:
         )
 
         try:
-            return experience_gain.experienceEvent.template.format(**tokens)
+            return experience_gain.experienceEvent.template.format_map(tokens)
         except KeyError as error:
             logging.error(error)
             return experience_gain.experienceEvent.template
